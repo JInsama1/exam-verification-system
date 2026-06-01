80fa709dde6b738a5871b4aad23b7f1f2b291577
Nikhil <nikhil@brightpeak.in>
2026-06-01 22:23:09 +0530

    Enable frontend CORS support

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,18 @@ async function bootstrap() {
     );
 
 
+  app.enableCors({
+
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+    ],
+
+
+    credentials: true,
+
+  });
+
 
   app.useGlobalInterceptors(
 
