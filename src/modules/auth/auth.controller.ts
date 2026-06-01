3c4f47d7584eaadf5488649a7db6e7edddc3297c
Nikhil <nikhil@brightpeak.in>
2026-06-01 15:48:57 +0530

    Add role based authorization

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,7 +10,12 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
+
 import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import { Roles } from '../../common/decorators/roles.decorator';
+
+import { Role } from '../../common/enums/role.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -28,11 +33,26 @@ export class AuthController {
     );
   }
 
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
   getMe(
     @CurrentUser() user: any,
   ) {
     return user;
+  }
+
+
+  @Get('master-test')
+  @UseGuards(
+    JwtAuthGuard,
+    RolesGuard,
+  )
+  @Roles(Role.MASTER_ADMIN)
+  masterTest() {
+    return {
+      message:
+        'Master admin access granted',
+    };
   }
 }
