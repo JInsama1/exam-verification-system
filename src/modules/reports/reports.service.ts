52815b62c4cbe041e3dd35f6aeab9d4b8799be33
Nikhil <nikhil@brightpeak.in>
2026-06-04 14:55:07 +0530

    Format report verification timestamp

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -178,7 +178,30 @@ export class ReportsService {
 
 
           verifiedAt:
-            item.verifiedAt,
+
+  item.verifiedAt
+    ?
+  item.verifiedAt.toLocaleString(
+    'en-IN',
+    {
+      day: '2-digit',
+
+      month: '2-digit',
+
+      year: 'numeric',
+
+      hour: '2-digit',
+
+      minute: '2-digit',
+
+      second: '2-digit',
+
+    },
+  )
+
+    :
+
+  '',
 
         }),
 
